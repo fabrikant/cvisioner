06398bb33bebdc7951fa2dda3af828661cd47481
Artem Banderoff <banderovaa@gmail.com>
2022-11-16 10:20:38 +0600

    _

--- visioner.py
+++ visioner.py
@@ -1,22 +1,67 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5 import uic, QtGui
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+from PyQt5.QtGui import QPixmap
+import cv2
+import numpy as np
+
+class VideoProcessor(QThread):
+
+    img_redy_signal = pyqtSignal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
+
+    def run(self):
+        # capture from web cam
+        cap = cv2.VideoCapture(-1)
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if ret:
+                self.img_redy_signal.emit(cv_img)
+        # shut down capture system
+        cap.release()
+
+    def stop(self):
+        """Sets run flag to False and waits for thread to finish"""
+        self._run_flag = False
+        self.wait()
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('ui/MainWindow.ui', self)
+        # self.buttonStartCapture = self.findChild(QPushButton, 'buttonStartCapture')
+        # self.buttonStartCapture.clicked.connect(self.on_mouse_click)
+        # self.startCapture()
 
-        self.buttonStartCapture = self.findChild(QPushButton, 'buttonStartCapture')
-        self.buttonStartCapture.clicked.connect(self.on_mouse_click)
+    def start_capture(self):
+        self.thread = VideoProcessor()
+        self.thread.img_redy_signal.connect(self.update_image)
+        self.thread.start()
 
-    def on_mouse_click(self):
-        print('mouse click')
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(cv_img)
+        self.videoFrame = self.findChild(QLabel, 'videoFrame')
+        self.videoFrame.setPixmap(qt_img)
+
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(self.videoFrame.width(), self.videoFrame.height(), Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
 
 
 if __name__ == "__main__":
-    print(sys.argv)
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
